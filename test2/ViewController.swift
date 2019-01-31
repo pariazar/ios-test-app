280b45d4f2b3aec19217088c6166570e3a882ffe
HamedPa <hamedpa@HamedPas-Mac.local>
2019-01-31 04:30:44 -0800

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by HamedPa on 1/31/19.
+//  Copyright © 2019 HamedPa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
